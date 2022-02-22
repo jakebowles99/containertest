d26b3b75d63b7ce3b640128bf416baf4561258e1
Jake Bowles <Jake.bowles@wirehive.com>
2022-02-22 17:55:17 +0000

    Working flask

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import json
 import os
 from azure.storage.blob import BlobServiceClient, BlobClient
 from azure.storage.blob import ContentSettings, ContainerClient
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
 
 athleteId = search_athletes(firstname="jake", surname="bowles")
 atheleteDetails = get_athlete(athleteId[0]["athlete_id"])
@@ -30,8 +33,8 @@ file.write(performances_json)
 file = open("/tmp/out/pbs.json","w")
 file.write(pbs_json)
 
-MY_CONNECTION_STRING = "BlobEndpoint=https://jbtestingpipeline.blob.core.windows.net/;QueueEndpoint=https://jbtestingpipeline.queue.core.windows.net/;FileEndpoint=https://jbtestingpipeline.file.core.windows.net/;TableEndpoint=https://jbtestingpipeline.table.core.windows.net/;SharedAccessSignature=sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2022-02-22T22:19:54Z&st=2022-02-22T14:19:54Z&spr=https&sig=O%2F8gPSPEPBU6iU06b4ioPflqYkavUbXaxAaYS9y%2B9%2B4%3D"
-MY_CONTAINER = "pythontst"
+MY_CONNECTION_STRING = "BlobEndpoint=https://jbconttest.blob.core.windows.net/;QueueEndpoint=https://jbconttest.queue.core.windows.net/;FileEndpoint=https://jbconttest.file.core.windows.net/;TableEndpoint=https://jbconttest.table.core.windows.net/;SharedAccessSignature=sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2022-02-23T01:28:40Z&st=2022-02-22T17:28:40Z&spr=https&sig=t4y5sYntDjayfb4NQ4B5rz%2FtW5TWEmCMPpVoYb4M1i8%3D"
+MY_CONTAINER = "container"
 LOCAL_PATH = "/tmp/out"
 
 class AzureBlobFileUploader:
@@ -64,7 +67,13 @@ class AzureBlobFileUploader:
       blob_client.upload_blob(data,overwrite=True)
  
  
-# Initialize class and upload files
-azure_blob_file_uploader = AzureBlobFileUploader()
-azure_blob_file_uploader.upload_all_images_in_folder()
+
 #print("Hello World!")
+
+@app.route('/')
+@app.route('/index')
+def hello_world():
+  # Initialize class and upload files
+    azure_blob_file_uploader = AzureBlobFileUploader()
+    azure_blob_file_uploader.upload_all_images_in_folder()
+    return render_template('index.html', title='Welcome', username='jake')
